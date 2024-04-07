378f437b8bf26cf0bf6898a3a2abc26dee8352e6
jmcquarrie <jmcquarrie@wfscorp.com>
2024-04-07 06:58:19 -0400

    update main

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, computed, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  counter = 0;
+
+  increment() {
+    this.counter++
+  }
+}
